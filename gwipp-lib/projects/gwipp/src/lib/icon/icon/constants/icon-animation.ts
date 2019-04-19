956de37c1e75383aff8f2561efb29113c9b88f4a
matt62king <matt62king@gmail.com>
2019-04-19 00:47:16 -0500

    GWIP-5 add icon component

---
+++ gwipp-lib/projects/gwipp/src/lib/icon/icon/constants/icon-animation.ts
@@ -0,0 +1,4 @@
+export class IconAnimation {
+  public static SPIN = 'fa-spin';
+  public static PULSE = 'fa-pulse';
+}
--- gwipp-lib/src/app/app.component.ts
+++ gwipp-lib/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IconSize, IconAnimation} from 'gwipp';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gwipp-lib';
+  iconSize = IconSize.TEN_X;
+  animation = IconAnimation.SPIN;
 }
