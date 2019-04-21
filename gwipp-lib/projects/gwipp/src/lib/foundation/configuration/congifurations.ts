a64e780ddbc0a21803706ce1899857a1a92612d8
matt62king <matt62king@gmail.com>
2019-04-21 00:46:58 -0500

    GWIP-7 add form configuration

--- gwipp-lib/projects/gwipp/src/lib/foundation/configuration/congifurations.ts
+++ gwipp-lib/projects/gwipp/src/lib/foundation/configuration/congifurations.ts
@@ -1,9 +1,26 @@
 import {ButtonConfigurationOptions} from '../../button/foundation/configuation/button-configuration-options';
 import {ButtonConfiguration} from '../../button/foundation/configuation/button-configuration';
+import {FormConfigurationOptions} from '../../form/foundation/configuration/form-configuration-options';
+import {FormConfiguration} from '../../form/foundation/configuration/form-configuration';
 
 export const Button = (options: ButtonConfigurationOptions) =>
-  function (target: Object, propertyKey: string) {
+  (target: Object, propertyKey: string) => {
     let value = new ButtonConfiguration(options);
+
+    function get() {
+      return value;
+    }
+
+    function set(newValue) {
+      value = newValue;
+    }
+
+    Object.defineProperty(target, propertyKey, {get, set});
+  };
+
+export const Form = (options: FormConfigurationOptions) =>
+   (target: Object, propertyKey: string) => {
+    let value = new FormConfiguration(options);
 
     function get() {
       return value;
