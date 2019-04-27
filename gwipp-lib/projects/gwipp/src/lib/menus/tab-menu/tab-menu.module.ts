f1adc2d798e19d269e79da35549fbc198845b3de
matt62king <matt62king@gmail.com>
2019-04-27 14:31:59 -0500

    GWIP-20 tab menu

--- gwipp-lib/projects/gwipp/src/lib/menus/tab-menu/tab-menu.module.ts
+++ gwipp-lib/projects/gwipp/src/lib/menus/tab-menu/tab-menu.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TabMenuComponent} from './tab-menu.component';
+import {StyleModule} from '../../foundation/style/style.module';
 
 @NgModule({
   declarations: [
     TabMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    StyleModule
   ],
   exports: [
     TabMenuComponent
